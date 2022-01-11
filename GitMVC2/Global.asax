f1598812d3dc220b0396d47aa4e42d0c52df110f
Pattarapol Srirak <ptrp.srirak@gmail.com>
2022-01-11 11:42:42 +0700

    Add project files.

---
+++ GitMVC2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GitMVC2.MvcApplication" Language="C#" %>
---
+++ GitMVC2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitMVC2.Startup))]
+namespace GitMVC2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
